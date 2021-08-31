2f6cda70696b30529fd5300b5f57cdb211cea9f8
zhengdf <zhengdf@qq.com>
2021-08-31 15:02:16 +0800

    modify rename.sh

--- rename.sh
+++ rename.sh
@@ -2,6 +2,6 @@
 
 ls | while read i;do
 mv ./$i/*barcodes.tsv.gz ./$i/barcodes.tsv.gz
-mv ./$i/*genes.tsv.gz ./$i/genes.tsv.gz
+mv ./$i/*genes.tsv.gz ./$i/features.tsv.gz
 mv ./$i/*matrix.mtx.gz ./$i/matrix.mtx.gz
 done
